3e81743c7e7fdf95c333dcd565566cf66d868ce2
xernom-gt <dasboard10@gmail.com>
2024-09-06 18:12:02 +0700

    kodingan yang menyatakan lulus

---
+++ kelulusan.py
@@ -0,0 +1,8 @@
+nama = input('MASUKAN NAMA ANDA :')
+data = int(input('MASUKAN NILAI ANDA :'))
+
+if data >= 75 :
+    print (f'SELAMAT ANANDA {nama} !! \nANDA DINYATAKAN LULUS DENGAN NILAI YANG DI PEROLEH {data} ')
+
+else : 
+    print (f'MAAF ANANDA {nama} \n ANDA DINYATAKAN TIDAK LULUS DENGAN NILAI YANG DI PEROLEH {data}')
