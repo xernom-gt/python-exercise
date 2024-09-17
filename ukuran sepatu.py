fea404f4586ae22dba61568bf13f7440865b0742
xernom-gt <dasboard10@gmail.com>
2024-09-17 10:54:11 +0700

    mmembuat kodingan ukuran sepatu

---
+++ ukuran sepatu.py
@@ -0,0 +1,9 @@
+data = int(input('berapa ukuran sepatu mu :'))
+
+if data >= 40 :
+    print ('ukuran sepatu mu besar')
+elif data >= 35 :
+    print ('ukuran sepatu mu sedang')
+
+else:
+    print ('ukuran sepatu mu kecil')
