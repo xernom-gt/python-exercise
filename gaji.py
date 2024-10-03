55e1f5b9c8f33a416ffbaa09df85e5427001f225
xernom-gt <dasboard10@gmail.com>
2024-10-03 09:18:01 +0700

    kodingannya

---
+++ gaji.py
@@ -0,0 +1,23 @@
+print ('='*40)
+print ('\t CALCULATOR')
+print ('='*40)
+
+s = int (input('nilai 1 :'))
+s1 = int (input('nilai 1 :'))
+x = input ('*,/,-,+')
+
+if x == '*' :
+    hasil = s * s1
+    print (hasil)
+elif x == '/' :
+    hasil = s * s1
+    print (hasil)
+elif x == '+' :
+    hasil = s * s1
+    print (hasil)
+elif x == '-' :
+    hasil = s * s1
+    print (hasil)
+
+else :
+    print ('silahkan ulangi')
