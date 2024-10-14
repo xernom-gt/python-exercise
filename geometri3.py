af7aec9d80b8f721ff5f4757afdc5779d388aafe
xernom-gt <dasboard10@gmail.com>
2024-10-14 15:54:33 +0700

    codingan geometri'

---
+++ geometri3.py
@@ -0,0 +1,12 @@
+def data():
+    print ('luas segitiga')
+    a = int(input('masukan alasnya   :'))
+    l = int(input('masukan tingginya :'))
+
+    la = lambda : 1/2 * a * l
+
+    print ('luas segitiga adalah :',int(la()) )
+
+data()
+data()
+data()
