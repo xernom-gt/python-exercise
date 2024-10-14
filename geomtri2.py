3b9c1d09a366f505a50b02ced793f1aaf76e0933
xernom-gt <dasboard10@gmail.com>
2024-10-14 09:32:34 +0700

    'geometri'

---
+++ geomtri2.py
@@ -0,0 +1,10 @@
+def persegi():
+    p = int(input('masukan nilai panjangnya :'))
+    l = int(input('masukan nilai lebarnya   :'))
+    t = int(input('masukan nilai tingginya  :'))
+
+    v = lambda : p*l*t
+    print (v())
+persegi()
+persegi()
+persegi()
