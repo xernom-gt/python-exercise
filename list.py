3664d241501873c10abf4c6c2e1dbb9fc8568769
xernom-gt <dasboard10@gmail.com>
2024-10-28 06:14:31 +0700

    looping

---
+++ list.py
@@ -0,0 +1,12 @@
+nama = [
+    'aden',
+    'aditya',
+    'ahmad'
+
+]
+
+nama [0] = 'aden fachri'
+nama.append('alifka')
+
+for i,x in enumerate (nama) :
+    print(i+1,'.',x)
---
+++ perkalian 1-10.py
@@ -0,0 +1,5 @@
+for i in range (1,11):
+    for y in range (1,11):
+        jumlah = i * y 
+        print (f'{y}x{i}={jumlah}',end=' | ')
+    print()
