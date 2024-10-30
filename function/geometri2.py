41a76fca2ad23806ecde5000770006f61879b4ea
xernom-gt <dasboard10@gmail.com>
2024-10-30 13:39:26 +0700

    volume kubus

---
+++ function/geometri2.py
@@ -0,0 +1,8 @@
+def data():
+    print ("volume kubus")
+    sisi = int(input("masukan nilai sisinya :"))
+    jumlah = lambda : sisi ** 3
+    print (f"volume kubusnya adalah {jumlah()} cm3")
+data()
+data()
+data()
