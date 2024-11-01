79f9469e0e090d5ead31bec3903c11f5740359aa
xernom-gt <dasboard10@gmail.com>
2024-11-01 08:52:12 +0700

    perulangan perkalian

--- looping/soal18(perkalian1-10).py
+++ looping/soal18(perkalian1-10).py
@@ -1,5 +1,5 @@
-for i in range (1,11):
-    for x in range (1,11):
+for i in range (1,5):
+    for x in range (1,5):
         jumlah = i * x
         print (f"{x} x {i} = {jumlah}",end=" \t")
     print()
