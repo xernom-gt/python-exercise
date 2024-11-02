caa09ae678dba4ea3ac863dbf43ac8678f6ecae9
xernom-gt <dasboard10@gmail.com>
2024-11-02 20:38:15 +0700

    volume limas segitiga

--- function/geometri4.py
+++ function/geometri4.py
@@ -1,7 +1,9 @@
 def data():
     print("volume limas segitiga")
+
     la = int(input("masukan nilai alasnya :"))
     lt = int(input("masukan nilai tingginya :"))
+    
     volume = lambda : 1/3 *la *lt
     print (f"volume limas segitiga adalah {int(volume())} cm3")
 data()
