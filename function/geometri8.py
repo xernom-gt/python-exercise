2d2a3944e69615afb16c18bf038b8991a24b1754
xernom-gt <dasboard10@gmail.com>
2024-11-12 11:45:53 +0700

    keliling layang layang

---
+++ function/geometri8.py
@@ -0,0 +1,14 @@
+def data():
+    print('='*35)
+    print("\t keliling layang layang")
+    print('='*35)
+
+    a = int(input('masukan nilai panjangnya :'))
+    b = int(input('masukan nilai panjangnya :'))
+
+    jum = lambda:2 * (a+b)
+
+    print (f'keliling layang layang adalah {int(jum())}')
+data()
+data()
+data()
