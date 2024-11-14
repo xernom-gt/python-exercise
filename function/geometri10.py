48f2f5fbbb520f53d039f4a87e9ccf63886fb116
xernom-gt <dasboard10@gmail.com>
2024-11-14 07:46:09 +0700

    keliling belah ketupat

---
+++ function/geometri10.py
@@ -0,0 +1,14 @@
+def data():
+    print('='*35)
+    print("\t keliling belah ketupat")
+    print('='*35)
+
+    s = int(input("masukan nilai sisinya :"))
+
+    jum = lambda: 4 * s
+
+    print (f"keliling belah ketupat adalah {jum()} cm2")
+
+data()
+data()
+data()
