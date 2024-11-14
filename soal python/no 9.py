4a9a9ec6d400be84ec153b98407ba8ea02d38a03
xernom-gt <dasboard10@gmail.com>
2024-11-14 21:04:52 +0700

    inputan

---
+++ soal python/no 9.py
@@ -0,0 +1,11 @@
+kelompok = input("kelompok :")
+asal = input("Asal daerah :")
+tim = input("Jumlah tim :")
+
+print (f"""
+===================================
+Kelompok \t: {kelompok}
+Asal daerah \t: {asal}  
+Jumlah tim \t: {tim}   
+===================================
+""")
