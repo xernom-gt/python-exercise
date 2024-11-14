bfa95c4b06ce3c1bad42c73d94acac51b97420cc
xernom-gt <dasboard10@gmail.com>
2024-11-14 20:19:11 +0700

    average and summary of uncorrected misstatements

---
+++ soal python/no 5 dan 6.py
@@ -0,0 +1,15 @@
+nilai_1 = 87.77
+nilai_2 = 66.50
+nilai_3 = 97.5
+
+jumlah = nilai_1 + nilai_2 + nilai_3
+rata_rata = nilai_1 + nilai_2 + nilai_3 / 3 
+
+print (f"""
+angka 1 : {nilai_1}
+angka 2 : {nilai_2}
+angka 3 : {nilai_3}
+
+jumlah : {jumlah}
+rata rata : {rata_rata}
+""")
