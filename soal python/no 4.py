87f305294b63023d496ac24ba40cf71ebc69131a
xernom-gt <dasboard10@gmail.com>
2024-11-14 20:05:54 +0700

    variable

---
+++ soal python/no 4.py
@@ -0,0 +1,13 @@
+Nama_Barang = "Kertas"
+Jumlah_Barang = 20
+Harga_Barang = 1000000
+
+ketersediaan = True
+
+print (f"""
+Nama Barang : {Nama_Barang}
+Jumlah Barang : {Jumlah_Barang}
+Harga Barang : {Harga_Barang}
+Ketersediaan : {ketersediaan}
+""")
+
