5f59422b7047aa12a8fad4f5a4129351fb1338cb
xernom-gt <dasboard10@gmail.com>
2024-11-14 07:17:10 +0700

    luas persegi

---
+++ function/geometri9.py
@@ -0,0 +1,12 @@
+def data():
+    print ('='*35)
+    print ('\tluas persegi')
+    print ('='*35)
+
+    s = int(input("masukan nilai sisi nya :"))
+    jum = lambda: s**2
+
+    print(f'nilai luas persegi adalah {jum()} cm2')
+data()
+data()
+data()
