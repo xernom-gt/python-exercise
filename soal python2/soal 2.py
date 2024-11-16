4400410f0fedf2cf27fc4fe90f768a40380d686a
xernom-gt <dasboard10@gmail.com>
2024-11-16 18:11:35 +0700

    memunculkan bulan dengan inputan menggunakan list

---
+++ soal python2/soal 2.py
@@ -0,0 +1,6 @@
+month = ["januari","februari","maret","april","mei","juni","juli","agustus","september","oktober","november","desember"]
+
+
+inputan = int(input("bulan ke berapa? "))
+
+print (f'bulan ke {inputan} adalah bulan {month[inputan-1]}')
