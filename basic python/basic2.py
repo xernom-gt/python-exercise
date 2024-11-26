582341af386370e00d9d0809fea61965485a83e7
xernom-gt <dasboard10@gmail.com>
2024-11-26 21:09:15 +0700

    variable

---
+++ basic python/basic2.py
@@ -0,0 +1,9 @@
+# What is a Variable?
+# A variable is a place to store data that is given a name so that we can use and manage that data in the program.
+# variables are like "containers" for storing values, and the values ​​inside them can be changed.
+
+container = True
+
+# to call variable just like this
+
+print (container)
