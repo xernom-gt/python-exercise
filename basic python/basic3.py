431b8260e61460f35087af607fdfca86c592d4db
xernom-gt <dasboard10@gmail.com>
2024-11-26 21:01:59 +0700

    data type

---
+++ basic python/basic3.py
@@ -0,0 +1,10 @@
+# data types are used to store simple values.
+# so data type have 4 elements
+integer = 25
+float = 3.5
+boolean = True
+string = "hello"
+print (type(string)) # string to store a set of text or characters
+print (type(integer)) # integer for whole, positive and negative numbers including zero
+print (type(float)) # float for decimal number
+print (type(boolean)) # the value is only True or False useful in programming logic (branching, looping)
