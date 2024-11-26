489d2bf6754dd3b77a583ad2a68e157300249b46
xernom-gt <dasboard10@gmail.com>
2024-11-26 21:27:41 +0700

    if/elif, else

---
+++ basic python/basic5.py
@@ -0,0 +1,14 @@
+# if/elif ,else are commands to determine whether the output is true or false with several actions
+
+CONS = input("hello how are you? ")
+
+if CONS == "i'm good" :
+    print ("i'm happy")
+
+elif CONS == "i'm not good" :
+    print ("get well soon")
+
+else :
+    print ("what?")
+
+# why do you have to == ? Actually, that's not all there is >, <, >= ,<= but that's in the next discussion .
