04607e7d99bcc344e69154e3cb2f562195923a4d
xernom-gt <dasboard10@gmail.com>
2024-11-26 21:55:59 +0700

    comparison operator

---
+++ basic python/basic6.py
@@ -0,0 +1,18 @@
+# Operator	|    Name	                  |         Explanation	                                                      |     Example
+# ==	    |   Equal to	              |  Checks if two values are equal.	                                      |      x == 5
+# !=	    |   Not equal to	          | Checks if two values are different.	                                      |      x != 5
+# >	        |   Greater than	          | Checks if the value on the left is greater than the one on the right.	  |      x > 5
+# <	        |   Less than	              | Checks if the value on the left is less than the one on the right.	      |      x < 5
+# >=	    |   Greater than or equal to  | Checks if the value on the left is greater than or equal to the right.	  |      x >= 5
+# <=	    |   Less than or equal to	  | Checks if the value on the left is less than or equal to the right.	      |      x <= 5
+
+# example 
+
+CONS = 16 
+CONTAINER = 17
+
+print (CONS == 16)
+
+print (CONS >= 17)
+
+print (CONS <= CONTAINER)
