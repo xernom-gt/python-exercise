5ca54c5cadfbbe593e82b5fb427778d94ede909a
xernom-gt <dasboard10@gmail.com>
2024-11-26 21:37:37 +0700

    input

---
+++ basic python/basic4.py
@@ -0,0 +1,15 @@
+# so input is input from the user, usually the input will be a string of numbers or decimals
+
+container = input("anything : ")
+
+print (container)
+
+# input can be changed in the input specifically to float, or integer
+
+CONS = int(input("1 - 10 : "))
+
+print (CONS)
+
+CONS = float(input('1.0 - 10.0 : '))
+
+print (CONS)
