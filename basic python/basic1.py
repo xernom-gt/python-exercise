1d8e1b4215611ff0bc37811eb9e3f62729161fe3
xernom-gt <dasboard10@gmail.com>
2024-11-26 20:46:58 +0700

    command to display output to screen monitor

---
+++ basic python/basic1.py
@@ -0,0 +1,15 @@
+print #print is a command to display input example
+
+print ("hello world")
+
+    # you must be asking why use () and "" or '' after entering print?
+    # quotation marks ('') or ("") are used to mark strings—that is, the set of characters you want to print or display
+
+# no 1, define strings
+# so this sign determines whether the input is program code or a variable
+
+print ('hello world') # this is plain text
+print (hello world) # this is an error, Python thinks "Hello" and "World" are variables
+
+# and in python there are 2 quotes like ' ' and " " which have their respective functions like '
+# # hello "world"' then the output is like hello "world" and vice versa
--- soal python2/soal 2.py
+++
@@ -1,6 +0,0 @@
-month = ["januari","februari","maret","april","mei","juni","juli","agustus","september","oktober","november","desember"]
-
-
-inputan = int(input("bulan ke berapa? "))
-
-print (f'bulan ke {inputan} adalah bulan {month[inputan-1]}')
