1fa8fc5a0ab5f047c4e035a2465a40e84178771a
xernom-gt <dasboard10@gmail.com>
2024-11-30 21:33:22 +0700

    menghapus elemen duplikat

---
+++ basic python/sum.py
@@ -0,0 +1,7 @@
+mlist = [10, 20, 30, 20, 50, 30, 70, 50]
+
+my_list_unik = list(set(mlist))
+print(f"""
+      Daftar sebelum di hapus elemen duplikat: {mlist}
+      Daftar setelah menghapus elemen duplikat: {my_list_unik}
+""")
