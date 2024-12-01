4113e9dfd09286281fc772c240312f35731be862
xernom-gt <dasboard10@gmail.com>
2024-12-01 20:48:48 +0700

    lirik balonku ada lima cuman posisinya aneh

---
+++ string/string18.py
@@ -0,0 +1,14 @@
+print ("""
+    balon ku ada 5\n
+       \trupa rupa warnanya\n
+       \t\tmerah, kuning, kelabu\n
+       \t\t\thijau muda dan biru\n
+       \t\t\t\tmeletus balon hijau\n
+DORRRRRRR!!!!!!!
+
+
+
+
+
+
+""")
