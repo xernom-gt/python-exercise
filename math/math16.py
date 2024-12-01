dc7ad62d6a1d3e71bfc27d972e2169a1c017a0cd
xernom-gt <dasboard10@gmail.com>
2024-12-01 20:39:21 +0700

    menghitung keliling segitiga

---
+++ math/math16.py
@@ -0,0 +1,5 @@
+a = float(input("Masukkan sisi a: "))
+b = float(input("Masukkan sisi b: "))
+c = float(input("Masukkan sisi c: "))
+keliling = a + b + c
+print(f"Keliling segitiga adalah: {keliling}")
