d2cfb66c234201f204d015fc7b659fdb41882475
xernom-gt <dasboard10@gmail.com>
2024-12-01 21:20:18 +0700

    menghitung hipotenusa segitiga siku-siku

---
+++ math/math20.py
@@ -0,0 +1,5 @@
+import math
+a = float(input("Masukkan panjang sisi a: "))
+b = float(input("Masukkan panjang sisi b: "))
+hipotenusa = math.sqrt(a**2 + b**2)
+print(f"Panjang hipotenusa segitiga adalah: {hipotenusa:.2f}")
