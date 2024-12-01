9835c904d2a990e135284ed7b3abe6e4fe8f33a7
xernom-gt <dasboard10@gmail.com>
2024-12-01 21:19:17 +0700

    menghitung luas trapesium

---
+++ math/math19.py
@@ -0,0 +1,5 @@
+a = float(input("Masukkan panjang sisi atas: "))
+b = float(input("Masukkan panjang sisi bawah: "))
+t = float(input("Masukkan tinggi trapesium: "))
+luas = 0.5 * (a + b) * t
+print(f"Luas trapesium adalah: {luas}")
